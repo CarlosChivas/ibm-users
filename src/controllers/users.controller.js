7f2de965fb3fe8928e2afa8ce92ac96702bb68c6
Carlos <carloschivas023468@gmail.com>
2022-04-18 18:18:21 -0500

    sameSite: true

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -25,7 +25,8 @@ usersCtrl.login = async (req, res) => {
                             console.log(token)
                             const cookieOptions = {
                                 expires: new Date(Date.now()+90*24*60*1000),
-                                httpOnly: true
+                                httpOnly: true,
+                                sameSite: 'None'
                             }
                             res.cookie('jwt', token, cookieOptions);
                             res.status(200).send("Inicio de sesion correcto")
